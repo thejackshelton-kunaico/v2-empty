897ad9042d9474ca4b5f0db8956f1e4847ea3ce9
jack shelton <me@jackshelton.com>
2024-10-30 12:10:47 -0500

    v2 starter

--- src/root.tsx
+++ src/root.tsx
@@ -1,38 +1,38 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from "@qwik.dev/core";
 import {
-  QwikCityProvider,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
+	QwikCityProvider,
+	RouterOutlet,
+	ServiceWorkerRegister,
+} from "@qwik.dev/router";
 import { RouterHead } from "./components/router-head/router-head";
-import { isDev } from "@builder.io/qwik/build";
+import { isDev } from "@qwik.dev/core/build";
 
 import "./global.css";
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Don't remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikCityProvider>
-      <head>
-        <meta charset="utf-8" />
-        {!isDev && (
-          <link
-            rel="manifest"
-            href={`${import.meta.env.BASE_URL}manifest.json`}
-          />
-        )}
-        <RouterHead />
-      </head>
-      <body lang="en">
-        <RouterOutlet />
-        {!isDev && <ServiceWorkerRegister />}
-      </body>
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<head>
+				<meta charset="utf-8" />
+				{!isDev && (
+					<link
+						rel="manifest"
+						href={`${import.meta.env.BASE_URL}manifest.json`}
+					/>
+				)}
+				<RouterHead />
+			</head>
+			<body lang="en">
+				<RouterOutlet />
+				{!isDev && <ServiceWorkerRegister />}
+			</body>
+		</QwikCityProvider>
+	);
 });
